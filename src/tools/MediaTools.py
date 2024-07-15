e7371ae621840108ce142d0b17413aea3c73b4ad
math-pixel <mathcb74@gmail.com>
2024-07-15 16:38:04 +0200

    feat(playlist picker) Add struture of playlist picker

--- src/tools/MediaTools.py
+++ src/tools/MediaTools.py
@@ -28,6 +28,62 @@ class MediaChecker():
             return None
 
 
+class PlaylistPicker():
+
+    def __init__(self) -> None:
+        pass
+
+    def getRandomUrl(self, urlPlaylist, platform):
+        match(platform):
+            case PLATFORM.YOUTUBE:
+                YoutubePlaylistPicker().getRandomUrl(urlPlaylist)
+            case PLATFORM.SPOTIFY:
+                SpotifyPlaylistPicker().getRandomUrl(urlPlaylist)
+            case _:
+                print("Platform is not available")
+
+    def getAllUrl(self, urlPlaylist, platform):
+        match(platform):
+            case PLATFORM.YOUTUBE:
+                YoutubePlaylistPicker().getAllUrl(urlPlaylist)
+            case PLATFORM.SPOTIFY:
+                SpotifyPlaylistPicker().getAllUrl(urlPlaylist)
+            case _:
+                print("Platform is not available")
+
+# ------------------------- Playlist picker platform ------------------------- #
+# Protocol
+class PlaylistPickerProtocol():
+
+    def getRandomUrl(self, urlPlaylist, platform):
+        pass
+
+    def getAllUrl(self, urlPlaylist, platform):
+        pass
+
+# Platform Picker
+class YoutubePlaylistPicker(PlaylistPickerProtocol):
+
+    def __init__(self) -> None:
+        pass
+
+    def getRandomUrl(self, urlPlaylist, platform):
+        pass
+
+    def getAllUrl(self, urlPlaylist, platform):
+        pass
+
+class SpotifyPlaylistPicker(PlaylistPickerProtocol):
+
+    def __init__(self) -> None:
+        pass
+
+    def getRandomUrl(self, urlPlaylist, platform):
+        pass
+
+    def getAllUrl(self, urlPlaylist, platform):
+        pass
+
 
 if __name__ == "__main__":
     MediaChecker.getPlatformFromUrl("https://www.youtube.com/watch?v=K8Gc5ys5Ba8")
