bb4b1f6e7efe26b24e094ab59075f8e9c8f9c978
math-pixel <mathcb74@gmail.com>
2024-07-16 12:24:52 +0200

    feat(mediaManager) YEY create a media Manager !

---
+++ src/base/Media.py
@@ -0,0 +1,43 @@
+from src.tools.Downloaders import AudioDownloaderManager
+from src.tools.MediaPlayer import GenericAudioPlayer
+from src.tools.MediaTools import MediaChecker, PlaylistPicker
+
+class MediaManager():
+
+    def __init__(self) -> None:
+        self.downloader = AudioDownloaderManager()
+        self.mediaPlayer = GenericAudioPlayer()
+        self.mediaChecker = MediaChecker
+        self.playlistPicker = PlaylistPicker()
+
+    def play(self, music_url):
+        musicPath = self.getValidMedia(music_url)
+        self.mediaPlayer.play(musicPath)
+
+
+    def stop(self):
+        self.mediaPlayer.stop()
+
+    def resume(self):
+        self.mediaPlayer.resume()
+
+    def pause(self):
+        self.mediaPlayer.pause()
+    
+    def goTo(self, timestamp):
+        self.mediaPlayer.goTo(timestamp)
+
+    def getValidMedia(self, data):
+        if self.mediaChecker.isFileExist(data):
+            # Play sound
+            return data
+        else:
+            # Try to download
+            if self.mediaChecker.isUrl(data):
+                platform = self.mediaChecker.getPlatformFromUrl(data)
+                if platform != None:
+                    return self.downloader.download(data, platform)
+                else:
+                    print("Error : wrong platform")
+            else:
+                print("Media is not valid")
