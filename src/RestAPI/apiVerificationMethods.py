733aeaa3643e5bf1aa537f4d4d62b1a71497b72a
math-pixel <mathcb74@gmail.com>
2024-07-19 16:57:38 +0200

    feat(api) create verif method

---
+++ src/RestAPI/apiVerificationMethods.py
@@ -0,0 +1,39 @@
+# ---------------------------------------------------------------------------- #
+#                                 POST METHODS                                 #
+# ---------------------------------------------------------------------------- #
+# For add data
+def add_alarm_verification(data):
+    return '', 200
+
+def add_days_verification(data):
+    return '', 200
+
+def add_actions_verification(data):
+    return '', 200
+# ---------------------------------------------------------------------------- #
+#                                  PUT METHODS                                 #
+# ---------------------------------------------------------------------------- #
+# For replace data
+def update_name_verification(data):
+    return '', 200
+
+def update_ringonce_verification(data):
+    return '', 200
+
+def update_isactivated_verification(data):
+    return '', 200
+
+def update_days_verification(data):
+    return '', 200
+
+def update_time_verification(data):
+    return '', 200
+
+def update_actions_verification(data):
+    return '', 200
+
+# ---------------------------------------------------------------------------- #
+#                                DELETE METHODS                                #
+# ---------------------------------------------------------------------------- #
+def delete_clock_verification(data):
+    return '', 200
