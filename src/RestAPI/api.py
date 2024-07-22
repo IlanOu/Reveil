b7fb4517fdc367598b17f4b1c0da1c04c27a00ad
math-pixel <mathcb74@gmail.com>
2024-07-22 16:17:47 +0200

    feat(verif api) wip verification data api

--- src/RestAPI/api.py
+++ src/RestAPI/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
+from apiVerificationMethods import *
 import json
-
+import uuid
 app = Flask(__name__)
 
 store = {}
@@ -12,11 +13,12 @@ with open("./assets/store.json", 'r') as f:
 # ---------------------------------------------------------------------------- #
 @app.route('/api/', methods=['GET'])
 def get_root():
+    # TODO connect webhook
     return "Connected", 200
 
 @app.route('/api/alarms', methods=['GET'])
 def get_alarms():
-    return jsonify(store)
+    return jsonify(store["alarms"])
 
 
 # ---------------------------------------------------------------------------- #
@@ -25,59 +27,91 @@ def get_alarms():
 # For add data
 @app.route('/api/alarms', methods=['POST'])
 def add_alarm():
-    print(request.get_json())
-    return '', 204
+    data = request.get_json()
+    if add_alarm_verification(data):
+        data["id"] = uuid.uuid4()
+        # TODO WEBHOOK
+        return 200
+    else:
+        return data, 400
 
 @app.route('/api/days', methods=['POST'])
 def add_days():
-    print(request.get_json())
-    return '', 204
+    data = request.get_json()
+    if add_days_verification(data):
+        return 200
+    else:
+        return data, 400
 
 @app.route('/api/actions', methods=['POST'])
 def add_actions():
-    print(request.get_json())
-    return '', 204
+    data = request.get_json()
+    if add_actions_verification(data):
+        return 200
+    else:
+        return data, 400
 # ---------------------------------------------------------------------------- #
 #                                  PUT METHODS                                 #
 # ---------------------------------------------------------------------------- #
 # For replace data
 @app.route('/api/name', methods=['PUT'])
 def update_name():
-    print(request.get_json())
-    return '', 204
+    data = request.get_json()
+    if update_name_verification(data):
+        return 200
+    else:
+        return data, 400
 
 @app.route('/api/ringonce', methods=['PUT'])
 def update_ringonce():
-    print(request.get_json())
-    return '', 204
+    data = request.get_json()
+    if update_ringonce_verification(data):
+        return 200
+    else:
+        return data, 400
 
 @app.route('/api/isactivated', methods=['PUT'])
 def update_isactivated():
-    print(request.get_json())
-    return '', 204
-
+    data = request.get_json()
+    if update_isactivated_verification(data):
+        return 200
+    else:
+        return data, 400
+    
 @app.route('/api/days', methods=['PUT'])
 def update_days():
-    print(request.get_json())
-    return '', 204
+    data = request.get_json()
+    if update_days_verification(data):
+        return 200
+    else:
+        return data, 400
 
 @app.route('/api/time', methods=['PUT'])
 def update_time():
-    print(request.get_json())
-    return '', 204
+    data = request.get_json()
+    if update_time_verification(data):
+        return 200
+    else:
+        return data, 400
 
 @app.route('/api/actions', methods=['PUT'])
 def update_actions():
-    print(request.get_json())
-    return '', 204
+    data = request.get_json()
+    if update_actions_verification(data):
+        return 200
+    else:
+        return data, 400
 
 # ---------------------------------------------------------------------------- #
 #                                DELETE METHODS                                #
 # ---------------------------------------------------------------------------- #
 @app.route('/api/clock', methods=['DELETE'])
 def delete_clock():
-    print(request.get_json())
-    return '', 204
+    data = request.get_json()
+    if delete_clock_verification(data):
+        return 200
+    else:
+        return data, 400
 
 def startAPI():
     app.run()
--- src/RestAPI/apiVerificationMethods.py
+++ src/RestAPI/apiVerificationMethods.py
@@ -3,37 +3,44 @@
 # ---------------------------------------------------------------------------- #
 # For add data
 def add_alarm_verification(data):
-    return '', 200
+    if "name" in data and "ringOnce" in data and "alreadyRingOnce" in data and "isActivated" in data and "days" in data and "time" in data and "actions" in data:
+        return True
+    else:
+        return False
 
 def add_days_verification(data):
-    return '', 200
+    days = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
+    if data in days:
+        return True
+    else:
+        return False
 
 def add_actions_verification(data):
-    return '', 200
+    return True
 # ---------------------------------------------------------------------------- #
 #                                  PUT METHODS                                 #
 # ---------------------------------------------------------------------------- #
 # For replace data
 def update_name_verification(data):
-    return '', 200
+    return True
 
 def update_ringonce_verification(data):
-    return '', 200
+    return True
 
 def update_isactivated_verification(data):
-    return '', 200
+    return True
 
 def update_days_verification(data):
-    return '', 200
+    return True
 
 def update_time_verification(data):
-    return '', 200
+    return True
 
 def update_actions_verification(data):
-    return '', 200
+    return True
 
 # ---------------------------------------------------------------------------- #
 #                                DELETE METHODS                                #
 # ---------------------------------------------------------------------------- #
 def delete_clock_verification(data):
-    return '', 200
+    return True
